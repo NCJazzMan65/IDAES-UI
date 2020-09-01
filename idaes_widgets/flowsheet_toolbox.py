21074a02a5a5e5e3455ac900db6f3996e1c2432f
Tim Fuller <tim.fuller65@gmail.com>
2020-08-31 20:44:05 -0400

    Periodic check-in

--- idaes_widgets/flowsheet_toolbox.py
+++ idaes_widgets/flowsheet_toolbox.py
@@ -105,10 +105,12 @@ class Toolbox(ttk.LabelFrame):
         self.reactorframe.grid(column=0, row=3, sticky=(N,S,E,W))
         self.reactorframe.columnconfigure(0, weight=1)
         self.reactorframe.rowconfigure(0, weight=0)
-        self.reactorframe.rowconfigure(1, weight=1)
+        self.reactorframe.rowconfigure(1, weight=0)
+        self.reactorframe.rowconfigure(2, weight=0)
+        self.reactorframe.rowconfigure(3, weight=1)
 
         # Add the equilibrium reactor
-        self.equiliconfile='../graphics/Equil_Icon_30x40.png'
+        self.equiliconfile = '../graphics/Equil_Icon_30x40.png'
         self.equilicon = PhotoImage(file=self.equiliconfile)
         self.equiltoolboxitem = ttk.Label(self.reactorframe, 
                                           text='Equilibrium', 
@@ -116,5 +118,25 @@ class Toolbox(ttk.LabelFrame):
                                           compound='left', 
                                           style='toolboxitemstyle.TLabel')
         self.equiltoolboxitem.grid(column=0, row=0, sticky=(W))
+
+        # Add the yield reactor
+        self.yieldiconfile = '../graphics/Yield_Icon_30x40.png'
+        self.yieldicon = PhotoImage(file=self.yieldiconfile)
+        self.yieldtoolboxitem = ttk.Label(self.reactorframe,
+                                          text='Yield',
+                                          image=self.yieldicon,
+                                          compound='left',
+                                          style='toolboxitemstyle.TLabel')
+        self.yieldtoolboxitem.grid(column=0, row=1, sticky=(W))
+
+        # Add the gibbs reactor
+        self.gibbsiconfile = '../graphics/Gibbs_Icon_30x40.png'
+        self.gibbsicon = PhotoImage(file=self.gibbsiconfile)
+        self.gibbstoolboxitem = ttk.Label(self.reactorframe,
+                                          text='Gibbs',
+                                          image=self.gibbsicon,
+                                          compound='left',
+                                          style='toolboxitemstyle.TLabel')
+        self.gibbstoolboxitem.grid(column=0, row=2, sticky=(W))
 
 
